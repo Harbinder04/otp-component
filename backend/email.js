41542491e0cf18a8116d38ea0b080d1f7f15b8ec
Harbinder Singh <harbindersingh621@gmail.com>
2025-01-02 04:33:01 +0530

    api is fixed and connected to frontend

--- backend/email.js
+++ backend/email.js
@@ -1,19 +1,29 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { Resend } from "resend";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+console.log(process.env.RESEND_API_KEY);
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-app.get("/send", async (req: Request, res: Response) => {
+app.get("/send", async (req, res) => {
   const { email } = req.query;
+  console.log(email);
+  const reandomOtp = Math.floor(1000 + Math.random() * 9000);
   const { data, error } = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
-    to: [email as string],
+    to: [email],
     subject: "hello world",
-    html: "<strong>it works!</strong>",
+    html: `<strong>it works!</strong> <br> Your OTP is ${reandomOtp}`,
   });
 
   if (error) {
+    console.log(error);
     return res.status(400).json({ error });
   }
 
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,13 +1,38 @@
-// import React from 'react'
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const handleChange = (e: any) => {
+    setEmail(e.target.value);
+  };
+
+  const handleClick = async () => {
+    const res = await fetch(`http://localhost:3000/send?email=${email}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).catch((error) => console.error(error));
+
+    if (res && res.status === 200) {
+      setRedirect(true);
+    }
+  };
+
+  if (redirect) {
+    return <Navigate to="/verify" />;
+  }
+
   return (
     <div className='container'>
-        <h1>Enter you email to get your otp</h1>
-        <input type="email" placeholder="Email" />
-        <button>Send OTP</button>
+      <h1>Enter your email to get your OTP</h1>
+      <input type="email" placeholder="Email" onChange={handleChange} />
+      <button onClick={handleClick}>Send OTP</button>
     </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
